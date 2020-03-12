43315f4d394693771dc56d4b33d972d916650e65
AstridSuhartono <astrid_sh@yahoo.com>
2020-03-12 22:05:36 +1030

    create clear local storage function

--- highscores.js
+++ highscores.js
@@ -6,3 +6,8 @@ highScoresList.innerHTML = highScores
     return `<li class="high-score">${score.name} - ${score.score}</li>`;
   })
   .join("");
+
+  function deleteScores(e){
+    e.preventDefault();
+    localStorage.clear();
+}
