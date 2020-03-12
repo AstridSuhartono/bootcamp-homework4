c36b8930896d91ea74ae92c86b1bc7a8f5c643e2
AstridSuhartono <astrid_sh@yahoo.com>
2020-03-12 13:44:57 +1030

    add some questions into the js-question.js in an array type and create the html page

---
+++ js-quiz.js
@@ -0,0 +1,13 @@
+var quizContainer = document.querySelector("#quiz");
+var resultsContainer = document.querySelector("#results");
+var submitButton = document.querySelector("#submit");
+
+function buildQuiz(){}
+
+function showResults(){}
+
+// display quiz right away
+buildQuiz();
+
+// on submit, show results
+submitButton.addEventListener('click', showResults);
